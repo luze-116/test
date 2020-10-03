c2a9ed9945611733f919da4a9b125448601ea4ee
luze-116 <70842677+luze-116@users.noreply.github.com>
2020-10-03 21:08:24 +0800

    Add files via upload

---
+++ guli_parent/service/service_edu/src/main/java/com/atguigu/eduservice/entity/EduTeacher.java
@@ -0,0 +1,53 @@
+package com.atguigu.eduservice.entity;
+
+import com.baomidou.mybatisplus.annotation.*;
+
+import java.util.Date;
+
+import java.io.Serializable;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+
+/**
+ * <p>
+ * 讲师
+ * </p>
+ *
+ * @author testjava
+ * @since 2020-02-24
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+@Accessors(chain = true)
+@ApiModel(value="EduTeacher对象", description="讲师")
+public class EduTeacher implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @ApiModelProperty(value = "ID")
+    @TableId(value = "id", type = IdType.ID_WORKER_STR)
+    private String id;
+
+    @ApiModelProperty(value = "用户名")
+    private String name;
+
+    @ApiModelProperty(value = "邮箱")
+    private String email;
+
+    @ApiModelProperty(value = "权限 1管理员 2测试员")
+    private Integer level;
+
+    @ApiModelProperty(value = "创建时间")
+    @TableField(fill = FieldFill.INSERT)
+    private Date gmtCreate;
+
+    @ApiModelProperty(value = "更新时间")
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    private Date gmtModified;
+
+
+}
